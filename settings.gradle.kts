bc2de8538a307e708b13cd077d1963431950421c
Dhruv Prakash <dhruvprakash075@gmail.com>
2026-07-02 14:35:51 +0530

    fix: add Apache Maven repository for poi-on-android dependency resolution

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // poishadow (centic9/poi-on-android)
+        maven { url = uri("https://repo.maven.apache.org/maven2/") }
+    }
+}
+rootProject.name = "OpenFiles"
+include(":app")
+include(":core:ui", ":core:data", ":core:common")
+include(":feature:browser", ":feature:gallery", ":feature:viewer")
